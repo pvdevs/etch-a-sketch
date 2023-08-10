d401a983d171f65861e4daf0bbeb0fd7984517e9
Paulo Victor <paulovictor.devs@gmail.com>
2023-08-10 19:10:08 -0300

    edit paint rules, now pixels only fill when click or click and drag

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 const container = document.querySelector('.container');
+//const grid = document.querySelectorAll('.pixel');
+//const row = document.querySelectorAll('.gridRow');
+
+
+let mouseDown = false
 
 function defineGrid(num) {    
     for(i = 0; i < num; i++) {
         const gridRow = document.createElement('div');
         gridRow.classList.add('gridRow');
         gridRow.id = `gridRow-${i}`;
-
         container.appendChild(gridRow);
     }
-
     for (let i = 0; i < num; i++) {
         for(let i = 0; i < num; i++) {
-            const gridUnity = document.createElement('div');
+            const pixel = document.createElement('div');
             const currentRow = document.getElementById(`gridRow-${i}`);
-            
-            gridUnity.classList.add('gridUnity');
-            currentRow.appendChild(gridUnity);
-            
-            gridUnity.addEventListener('mouseenter', () => {
-            gridUnity.classList.add('gridFill');
-            })           
-        }
+
+            pixel.classList.add('pixel');
+            currentRow.appendChild(pixel);
+
+            pixel.addEventListener('dragstart', (event) => {
+                event.preventDefault(); // Prevent default drag behavior
+            });
+
+            pixel.addEventListener('mousedown', () => {
+                mouseDown = true;
+                pixel.classList.add('pixelFill');
+            });
+
+            pixel.addEventListener('mouseover', () => {
+                if (mouseDown) {
+                    pixel.classList.add('pixelFill');
+                }
+            });
+    
+        }        
     }
 
+
+    document.addEventListener('mouseup', () => {
+        mouseDown = false;
+    });
 }
 
 
-defineGrid(24);
+defineGrid(16);
