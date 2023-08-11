82c92082850265aaed66f7a47100849b4e655c64
Paulo Victor <paulovictor.devs@gmail.com>
2023-08-11 01:01:44 -0300

    add both rainbow and erase features

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
 const container = document.querySelector('.container');
 const rangeValue = document.querySelector('.range-value');
 const rangeInput = document.querySelector('#range');
+const rainbowButton = document.querySelector('#rainbow');
+const eraserButton = document.querySelector('#eraser');
+const clearButton = document.querySelector('#clear');
 
 let mouseDown = false;
 let rangeClick = false;
+let rainbow = false;
+let eraser = false;
+
+rainbowButton.addEventListener('click', (e) => {
+    rainbow = true;
+    eraser = false;
+})
+
+eraserButton.addEventListener('click', (e) => {
+    eraser = true;
+    rainbow = false;
+})
+
+// Not working right
+clearButton.addEventListener('click', (e) => {
+    container.replaceChildren();
+})
+
+
+function getRandomColor() {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
+}
+
+
+function getRainbow(pixel, cb) {
+    pixel.setAttribute('style', `background-color: #${cb}`);
+}
+
+
+function getErased(pixel) {
+   pixel.style.backgroundColor = '#FFF';
+}
+
+
+
 
 function paintGrid(pixel) {
     // This event is used to prevent users from dragging the grid when painting, in some tests it led to undesired behavior
@@ -19,7 +58,10 @@ function paintGrid(pixel) {
     // This event is used to keep painting when users keep pressing the click while pass through different pixels
     pixel.addEventListener('mouseover', () => {
         if (mouseDown) {
+            if(rainbow) getRainbow(pixel, getRandomColor());
+            else if(eraser) getErased(pixel);
             pixel.classList.add('pixelFill');
+            // pixel.setAttribute('style', `background-color: #${getRandomColor()}`) rainbow
         }
     });
 
@@ -27,6 +69,9 @@ function paintGrid(pixel) {
         mouseDown = false;
     });
 }
+
+
+
 
 function setGrid(num) {    
 
@@ -62,11 +107,13 @@ function setGridRange() {
 
     })
     rangeInput.addEventListener('mouseup', () => {
+        
+        // This is used to reset the container
         container.replaceChildren();
         setGrid(rangeInput.value)
     });
     
-    console.log(rangeInput.value);
+    console.log(rangeInput.value); 
     return rangeInput.value;
 }
 
