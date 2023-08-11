77f2602ed9f5fa5387dab82d41552089c6f5b9d7
Paulo Victor <paulovictor.devs@gmail.com>
2023-08-10 21:28:31 -0300

    add range bar to define grid

--- script.js
+++ script.js
@@ -1,48 +1,74 @@
 const container = document.querySelector('.container');
-//const grid = document.querySelectorAll('.pixel');
-//const row = document.querySelectorAll('.gridRow');
+const rangeValue = document.querySelector('.range-value');
+const rangeInput = document.querySelector('#range');
 
+let mouseDown = false;
+let rangeClick = false;
 
-let mouseDown = false
+function paintGrid(pixel) {
+    // This event is used to prevent users from dragging the grid when painting, in some tests it led to undesired behavior
+    pixel.addEventListener('dragstart', (event) => {
+        event.preventDefault(); 
+    });
 
-function defineGrid(num) {    
-    for(i = 0; i < num; i++) {
-        const gridRow = document.createElement('div');
-        gridRow.classList.add('gridRow');
-        gridRow.id = `gridRow-${i}`;
-        container.appendChild(gridRow);
-    }
-    for (let i = 0; i < num; i++) {
-        for(let i = 0; i < num; i++) {
-            const pixel = document.createElement('div');
-            const currentRow = document.getElementById(`gridRow-${i}`);
+    pixel.addEventListener('mousedown', () => {
+        mouseDown = true;
+        pixel.classList.add('pixelFill');
+    });
 
-            pixel.classList.add('pixel');
-            currentRow.appendChild(pixel);
-
-            pixel.addEventListener('dragstart', (event) => {
-                event.preventDefault(); // Prevent default drag behavior
-            });
-
-            pixel.addEventListener('mousedown', () => {
-                mouseDown = true;
-                pixel.classList.add('pixelFill');
-            });
-
-            pixel.addEventListener('mouseover', () => {
-                if (mouseDown) {
-                    pixel.classList.add('pixelFill');
-                }
-            });
-    
-        }        
-    }
-
+    // This event is used to keep painting when users keep pressing the click while pass through different pixels
+    pixel.addEventListener('mouseover', () => {
+        if (mouseDown) {
+            pixel.classList.add('pixelFill');
+        }
+    });
 
     document.addEventListener('mouseup', () => {
         mouseDown = false;
     });
 }
 
+function setGrid(num) {    
 
-defineGrid(16);
+    // This loop creates the rows
+    for(i = 0; i < num; i++) {
+        const gridRow = document.createElement('div');
+        gridRow.classList.add('gridRow');
+        gridRow.id = `gridRow-${i}`;
+        container.appendChild(gridRow);
+    }
+
+    // This loop select the rows to fill
+    for (let i = 0; i < num; i++) {          
+        
+        // This loop fills the selected row with the right amount of pixels
+        for(let i = 0; i < num; i++) {       
+            const pixel = document.createElement('div');
+            const currentRow = document.getElementById(`gridRow-${i}`);
+
+            pixel.classList.add('pixel');
+            currentRow.appendChild(pixel);
+            
+            paintGrid(pixel);
+        }        
+    }
+}
+
+function setGridRange() {
+    rangeValue.textContent = `${rangeInput.value} x ${rangeInput.value}`;
+
+    rangeInput.addEventListener('input', (e) => {
+        rangeValue.textContent = `${e.target.value} x ${e.target.value}`;
+
+    })
+    rangeInput.addEventListener('mouseup', () => {
+        container.replaceChildren();
+        setGrid(rangeInput.value)
+    });
+    
+    console.log(rangeInput.value);
+    return rangeInput.value;
+}
+
+
+setGrid(setGridRange());
